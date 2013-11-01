3bcabd7c2664448075a5e673f422b30067174344
Glay Guo <glay@Glays-MacBook-Pro.local>
2013-11-01 14:13:03 +0800

    修改：      log/error.log

--- themes/soonmes_member/user_login.html.php
+++ themes/soonmes_member/user_login.html.php
@@ -23,44 +23,64 @@
 </script>
 <form name="login" method="post" action="" id="log_in" onSubmit="submitonce(this)">
 <div id="main" class="clearfix">
-	<div class="box">
-		<div class="box-con" style=" position: relative;">
-			<div class="reg-header"><h3>用户登录</h3></div>
-			<div  style="width:540px; ">
-			<ul class="reg-table">
-				<li>
-					<div class="reg-l-title">用户名：</div>
-					<div class="reg-l-input">
-						<input type="text"  id="username" name="username" maxlength="64" style="color:#999" tabindex="1" >
-					</div>
-				</li>
-				<li>
-					<div class="reg-l-title">密码：</div>
-					<div class="reg-l-input"><input type="password"  name="password" id="password" maxlength="16" tabindex="2" /></div>
-					 
-				</li>
-                                <li>
-					<div class="reg-l-title">动态口令：</div>
-					<div class="reg-l-input"><input type="text"  name="uchoncode"  maxlength="16" tabindex="2" /></div>
-					 
-				</li>
-				<li>
-					<div class="reg-l-title"></div>
-					<div class="reg-l-input"><table><tr><td>记住我</td><td><input type="checkbox" name="remember" value="1" style="width:15px;border:0" /></td><td><select name="save_time"><option value="3600">一个小时</option><option value="18000">五个小时</option><option value="86400">一天</option><option value="604800">一周</option><option value="2592000">一个月</option><option value="31104000">永远</option></select></td></tr></table></div>
-				</li>
-				<li>
-					<div class="reg-l-title"> </div>
-					<div  ><input type="submit" value="立即登录" class="btn-action" />
-					</div>
-					<div class="reg-l-title"><a href="/index.php?user&q=going/getpwd" style="text-decoration:underline;color:#03c;">忘记密码?</a></div>
-                </li>
-			</ul>
-			</div>
-			<div style="width: 350px; position: absolute; right: 20px; top: 90px;">
-			<span>还没有帐号? <a href="/index.php?user&q=going/getreg" style="text-decoration:underline;color:#03c;">免费注册</a></span><br/><br/>
+		<!-- 主要内容 -->
+			<div class="content">
+				<!-- 2列 -->
+                <!-- 两栏结构-左为主要内容-右为辅助内容 -->
+                <div class="grid_c2_c20s4 clearfix m_b_25">
+                  <div class="col_main">
+                    <div class="main_wrap">
+                    	<!-- 左栏 -->
+                        <div class="loginModule">
+                        	<div class="pass_login_div">
+			
+							  <p class="item nameBg"><input id="username" name="username" maxlength="64" tabindex="1" type="text" class="input_name" value="请输入用户名"></p>
+                              <p class="item passBg"><input type="password"  name="password" id="password" maxlength="16" tabindex="2" class="input_pass" value="请输入密码"></p>
+                              <p class="item"><table><tr><td>记住我</td><td><input type="checkbox" name="remember" value="1" style="width:15px;border:0" /></td><td><select name="save_time"><option value="3600">一个小时</option><option value="18000">五个小时</option><option value="86400">一天</option><option value="604800">一周</option><option value="2592000">一个月</option><option value="31104000">永远</option></select></td></tr></table></p>
+                			  
+                			  <br>
+                			  <p class="item tc">
+                                	<input type="submit" class="btn btn_login m_r_25" value="登 录"</>
+                                	
+									<a href="/index.php?user&q=going/getreg" class="link">注 册</a>			 
+							  </p>
+							  <p class="item">
+							  <a href="/index.php?user&q=going/getpwd" class="link">忘记密码?</a>
+                                    
+							  </p>
+						
+					     </div>
+
 			</div>
 		</div>
-	</div>
+		</div>
+	<div class="col_extra">
+                  	<!-- 右侧 -->
+                    <div class="banner_login m_b_50">
+                    	<img src="{$tempdir}/images/banner_login.jpg" />
+                    </div>
+                    <div class="clearfix down">
+                   	  <div class="fl phoneDown">
+                       	<h4 class="m_b_10">中硕信投客户端下载</h4>
+                          <p>
+                             <a href="#"><img src="{$tempdir}/images/icon_android.png"></a>
+                             <a href="#"><img src="{$tempdir}/images/icon_iPhone.png"></a>
+                          </p>
+                        </div>
+                        <div class="fl club"><a href="#"><img src="{$tempdir}/images/club.jpg"></a></div>
+                   	  <div class="fl weixin">
+                        	<h4 class="tit m_b_10">官方微信</h4>
+                            <p>微信扫描二维码，<br>获得每日精选资讯</p>
+                        </div>
+                    </div>
+                    <!-- end 右侧 -->
+                  </div>
+                  <!-- end -->
+                </div>
+                <!-- end 两栏结构-左为主要内容-右为辅助内容 -->
+				<!-- end 2列 -->
+			</div>
+			<!-- end 主要内容 -->		
 </div>
 </form>
 {include file="user_footer.html" }
--- themes/soonmes_member/user_reg_info.html.php
+++ themes/soonmes_member/user_reg_info.html.php
@@ -2,7 +2,8 @@
 !defined('IN_TEMPLATE') && exit('Access Denied');
 ?>
 {include file="user_header.html" }
-<script type="text/javascript" src="{$tempdir}/media/js/jquery.pstrength.js"></script>
+<script type="text/javascript"
+	src="{$tempdir}/media/js/jquery.pstrength.js"></script>
 {literal}
 <script>
  var childWindow;
@@ -18,69 +19,182 @@ jQuery(function(){
 });
 </script>
 <style>
-#passwordStrengthDiv{margin-top:6px;}
-.is0{background:url(/data/images/base/progress.png) no-repeat 0 0;}
-.is10{background-position:0 -7px;}
-.is20{background-position:0 -14px;}
-.is30{background-position:0 -21px;}
-.is40{background-position:0 -28px;}
-.is50{background-position:0 -35px;}
-.is60{background-position:0 -42px;}
-.is70{background-position:0 -49px;}
-.is80{background-position:0 -56px;}
-.is90{background-position:0 -63px;}
-.is100{background-position:0 -70px;}
+#passwordStrengthDiv {
+	margin-top: 6px;
+}
+
+.is0 {
+	background: url(/data/images/base/progress.png) no-repeat 0 0;
+}
+
+.is10 {
+	background-position: 0 -7px;
+}
+
+.is20 {
+	background-position: 0 -14px;
+}
+
+.is30 {
+	background-position: 0 -21px;
+}
+
+.is40 {
+	background-position: 0 -28px;
+}
+
+.is50 {
+	background-position: 0 -35px;
+}
+
+.is60 {
+	background-position: 0 -42px;
+}
+
+.is70 {
+	background-position: 0 -49px;
+}
+
+.is80 {
+	background-position: 0 -56px;
+}
+
+.is90 {
+	background-position: 0 -63px;
+}
+
+.is100 {
+	background-position: 0 -70px;
+}
 </style>
 {/literal}
-<link href="{$tempdir}/media/css/modal.css" rel="stylesheet" type="text/css" />
-<form action="" method="post" name="formUser" onSubmit="return userReg();" id="reg_sub">
-<div id="main" class="clearfix">
-	<div class="box">
-		<div class="box-con">
-			<p class="reg-pro">注册步骤：<span>1.填写注册信息</span> > 2.邮箱认证 > 3.上传头像> 4.注册完成</a></p>
-			<div class="reg-header"><h3>免费注册</h3><span ><a href="/index.action?user&q=action/login">已有帐号? 登录</a></span></div>
-			<ul class="reg-table" >
-				<li>
-					<div class="reg-l-title">用户名：</div>
-					<div class="reg-l-input"><input id="username" name="username" type="text" size="22" maxlength="10" value="" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';checkUsername(this.value);"></div>
-					<div class="reg-l-tips" id="username_notice"><span>*</span> 请输入4-10位字符.英文,数字</div>
-				</li>
-				<li>
-					<div class="reg-l-title">密码：</div>
-					<div class="reg-l-input"><input id="password" name="password" type="password" size="22" maxlength="20" value="" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';checkPassword(this.value);" ></div>
-					 <div id="passwordStrengthDiv" class="is0" style="width:138px;height:7px;overflow:hidden;"></div>    
-					<div class="reg-l-tips" id="password_notice"><span>*</span> 请输入6到20位密码,建议英文大小写+数字</div>
-				</li>
-				<li>
-					<div class="reg-l-title">确认密码：</div>
-					<div class="reg-l-input"><input id="conform_password" name="confirm_password" type="password" size="22" maxlength="20" value="" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';checkConformPassword(this.value);"></div>
-					<div class="reg-l-tips" id="conform_password_notice"><span>*</span> 请重复输入上面的密码</div>
-				</li>
-				<li>
-					<div class="reg-l-title">Email：</div>
-					<div class="reg-l-input"><input id="email" name="email" type="text" size="22" maxlength="30" value="" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';checkEmail(this.value);"></div>
-					<div class="reg-l-tips" id="email_notice"><span>*</span> 请输入您常用的邮箱地址,会邮箱认证</div>
-				</li>
-				<li>
-					<div class="reg-l-title">真实姓名：</div>
-					<div class="reg-l-input"><input id="realname" name="realname" type="text" size="22" maxlength="10" value="" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';checkRealname(this.value);"></div>
-					<div class="reg-l-tips" id="realname_notice"><span>*</span> (此处可不填)请填写您的真实姓名</div>
-				</li>
-				<li>
-					<div class="reg-l-title">介绍人：</div>
-					<div class="reg-l-input"><input id="invite_username" name="invite_username" type="text" size="22" maxlength="10" value="{$magic.session.reginvite_user_Name}" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';" ></div>
-					<div class="reg-l-tips"><span>*</span> (此处可不填)如填写介绍人的本站用户名(非真实姓名)，对方将获得奖励收入.<a href="#" id="infoyq" data-content="如果您申请了VIP并且成功付费，那么您的介绍人可以一次性获得100元的奖励。
-每月结算已付费的VIP提成，通过网站充值方式打到您的介绍人的账上。">邀请规则</a></div>
-				</li>
-				<li class="reg-li"><input type="checkbox" id="dianji" checked="checked" style="position:relative; top:3px; margin-right:5px;">我已阅读并且同意<a href="/data/reg.doc">协议</a></li>
-				<li class="reg-li"><input type="submit" id="reg_btn" class="btn-action" value="立即注册" /></li>
-			</ul>
-			<div style="width:480px">
-			
+<!-- <link href="{$tempdir}/media/css/modal.css" rel="stylesheet" type="text/css" /> -->
+<form action="" method="post" name="formUser"
+	onSubmit="return userReg();" id="reg_sub">
+	<div id="main" class="clearfix">
+
+		<div class="content">
+			<div class="register clearfix m_b_25">
+
+				<div class="hd m_b_20">
+					注册<s class="tip"></s>
+				</div>
+				<div class="bg clearfix">
+					<div class="fl regTB">
+						<table width="100%" class="tb_reg m_b_20">
+							<tr>
+							<tr>
+								<th>邮 箱：</th>
+								<td><div class="input_style01">
+										<span class="r"><input id="email" name="email" type="text"
+											size="22" maxlength="30" value=""
+											onFocus="this.className='biankuang1';"
+											onBlur="this.className='biankuang2';checkEmail(this.value);">
+									
+									</div>
+									<div class="reg-l-tips" id="email_notice">
+										<span>*</span> 请输入您常用的邮箱地址,进行邮箱认证
+									</div></td>
+							</tr>
+								<th>用户名：</th>
+								<td><div class="input_style01">
+										<span class="r"><input id="username" name="username"
+											type="text" size="22" maxlength="20" value=""
+											onFocus="this.className='biankuang1';"
+											onBlur="this.className='biankuang2';checkUsername(this.value);"></span>
+									</div>
+									<div class="reg-l-tips" id="username_notice">
+										<span>*</span> 请输入4-10位字符.英文,数字
+									</div></td>
+
+							</tr>
+							<tr>
+
+								<th>密 码：</th>
+								<td><div class="input_style01">
+										<span class="r"><input id="password" name="password"
+											type="password" size="22" maxlength="20" value=""
+											onFocus="this.className='biankuang1';"
+											onBlur="this.className='biankuang2';checkPassword(this.value);"></span>
+									</div>
+									<div id="passwordStrengthDiv" class="is0"
+										style="width: 138px; height: 7px; overflow: hidden;"></div>
+									<div class="reg-l-tips" id="password_notice">
+										<span>*</span> 请输入6到20位密码,建议英文大小写+数字
+									</div></td>
+								</li>
+							
+							
+							<tr>
+								<th>确认密码：</th>
+								<td><div class="input_style01">
+										<span class="r"><input id="conform_password"
+											name="confirm_password" type="password" size="22"
+											maxlength="20" value=""
+											onFocus="this.className='biankuang1';"
+											onBlur="this.className='biankuang2';checkConformPassword(this.value);"></span>
+									</div>
+									<div class="reg-l-tips" id="conform_password_notice">
+										<span>*</span> 请重复输入上面的密码
+									</div></td>
+							</tr>
+							
+<!-- 							<tr> -->
+								<!--  <th style="vertical-align: top;">验证码：</th>-->
+
+<!-- 								<td><div class="input_style01 m_r_10"> -->
+<!-- 										<span class="r"><input name="valicode" type="text" size="11" -->
+<!-- 											maxlength="4" />&nbsp;<img src="plugins/index.php?q=imgcode" -->
+<!-- 											alt="看不清换一张？" -->
+<!-- 											onClick="this.src='plugins/index.php?q=imgcode&t=' + Math.random();" -->
+<!-- 											align="absmiddle" style="cursor: pointer" /></span> -->
+<!-- 									</div></td> -->
+<!-- 							</tr> -->
+
+							<!-- 				<li> -->
+							<!-- 					<div class="reg-l-title">真实姓名：</div> -->
+							<!--  <div class="reg-l-input"><input id="realname" name="realname" type="text" size="22" maxlength="10" value="" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';checkRealname(this.value);"></div>-->
+							<!-- 					<div class="reg-l-tips" id="realname_notice"><span>*</span> (此处可不填)请填写您的真实姓名</div> -->
+							<!-- 				</li> -->
+							<!-- 				<li> -->
+							<!-- 					<div class="reg-l-title">介绍人：</div> -->
+							<!--  <div class="reg-l-input"><input id="invite_username" name="invite_username" type="text" size="22" maxlength="10" value="{$magic.session.reginvite_user_Name}" onFocus="this.className='biankuang1';"  onBlur="this.className='biankuang2';" ></div>-->
+							<!-- 					<div class="reg-l-tips"><span>*</span> (此处可不填)如填写介绍人的本站用户名(非真实姓名)，对方将获得奖励收入.<a href="#" id="infoyq" data-content="如果您申请了VIP并且成功付费，那么您的介绍人可以一次性获得100元的奖励。 -->
+							<!-- 每月结算已付费的VIP提成，通过网站充值方式打到您的介绍人的账上。">邀请规则</a></div> -->
+							<!-- 				</li> -->
+							<!--  <li class="reg-li"><input type="checkbox" id="dianji" checked="checked" style="position:relative; top:3px; margin-right:5px;">我已阅读并且同意<a href="/data/reg.doc">协议</a></li>-->
+							<!-- 				<li class="reg-li"><input type="submit" id="reg_btn" class="btn-action" value="立即注册" /></li> -->
+						
+							
+							
+						</table>
+						<p style="padding-left: 30px; margin-bottom: 20px;">
+							<input type="checkbox" id="dianji" checked="checked"></>我已阅读并且同意助贷网-个人信用贷款 小额贷款 投资理财 企业融资 的 <a href="/data/reg.doc">使用协议</a>
+						</p>
+						<p style="padding-left: 110px;">
+							<input type="submit" id="reg_btn" class="btn btn_login"
+								value="注 册" />
+						</p>
+					</div>
+					<div class="fl regInfo">
+						<h3 class="tit">您还没有注册?</h3>
+						<ol class="list m_b_40">
+							<li>1、帮助他人 快乐自己 收获利息</li>
+							<li>2、助您创业 资金周转 分期偿还</li>
+							<li>3、收益稳定可靠回报高</li>
+							<li>4、交易安全快捷有保障</li>
+						</ol>
+						<p style="padding-left: 25px;">
+							<label class="f_14 m_r_10">已有账号？</label> <a
+								href="/index.action?user&q=action/login"
+								class="ico_sprite btn btn_view_02">直接登录</a>
+						</p>
+					</div>
+
+				</div>
 			</div>
 		</div>
-	</div>
-</div>
+
 </form>
 {literal}
 <script type="text/javascript">
@@ -96,7 +210,7 @@ jQuery('#reg_btn').click(function(){
 });
 </script>
 {/literal}
-<script type="text/javascript"  src="{$tempdir}/media/js/user2.js"></script>
+<script type="text/javascript" src="{$tempdir}/media/js/user2.js"></script>
 <script src="{$tempdir}/media/js/tooltip.js"></script>
 <script src="{$tempdir}/media/js/popover.js"></script>
 {include file="user_footer.html"}
