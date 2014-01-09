50f8c23f384c467a789409b3f3eb80ec9343fcae
glayyiyi <glay.guo@gmail.com>
2014-01-09 11:31:22 +0800

    u

--- modules/payment/paytype/wyzx.class.php
+++ modules/payment/paytype/wyzx.class.php
@@ -1,9 +1,11 @@
 <?php
 class wyzxPayment {
 	public static function ToSubmit($payment){
-		$v_mid = '22869932';
-		$v_url = 'http://www.viptouzi.com/modules/payment/wyzx_return.php';
+		$v_mid = '22869932'; // 1001是网银在线的测试商户号，商户要替换为自己的商户号。
+		$v_url = 'http://www.viptouzi.com/modules/payment/wyzx_return.php';// 商户自定义返回接收支付结果的页面。对应Receive.php示例。
 		$key   = 'chang_43420024420';
+		$remark2 = '[url:=http://www.viptouzi.com/modules/payment/wyzx_return.php]'; //服务器异步通知的接收地址。对应AutoReceive.php示例。必须要有[url:=]格式。
+		
 		
 		
 		if(trim($payment['trade_no'])<>"")
